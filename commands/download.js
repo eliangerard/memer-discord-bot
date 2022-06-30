4380480fbcf0cd1e66862c4320dab3013d95bed5
Elian <eliangerardiso@gmail.com>
2022-06-30 17:21:41 -0600

    Cleaning console logs

--- commands/download.js
+++ commands/download.js
@@ -6,8 +6,6 @@ let jsoner = { json: true };
 
 const download = (uri, filename, callback) => {
     request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
   };
