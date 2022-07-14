a661329ca5ec280472f066c03147ea3d17c9e6f0
Elian <eliangerardiso@gmail.com>
2022-07-13 21:50:37 -0600

    Added pages based queue

--- commands/queue.js
+++ commands/queue.js
@@ -7,8 +7,8 @@ let header;
 const status = queue => `Volumen: \`${queue.volume}%\` | Filtro: \`${queue.filters.join(", ") || "Off"}\` | Repitiendo: \`${queue.repeatMode ? queue.repeatMode === 2 ? "Toda la cola" : "Esta canción" : "Nada"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
 
 const updateQueue = (queue) => {
-    header = `**Reproduciendo:** ${queue.songs[0].name} - \`${queue.songs[0].formattedDuration}\`\n ${status(queue)}\n\n`;
-    q = ``;
+    header = `**Reproduciendo:** ${queue.songs[0].name} - \`${queue.songs[0].formattedDuration}\`\n ${status(queue)}`;
+    q = `\n`;
     for(let i = page*10+1; i < (queue.songs.length > (page*10)+11 ? page*10+11 : queue.songs.length); i++)
         q += `**${i}.** ${queue.songs[i].name} - \`${queue.songs[i].formattedDuration}\`\n`;
     q += `\n*Página: ${(page+1)+"/"+totalPages}*`;
