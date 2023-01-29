94ddc6126fa8cccaa0c2a50e6787fc8fa75ca005
Elian <eliangerardiso@gmail.com>
2023-01-28 23:16:05 -0600

    quien sabe

--- commands/secret.js
+++ commands/secret.js
@@ -15,7 +15,7 @@ module.exports = {
         };
 
         fetch("https://sii.chihuahua2.tecnm.mx/modulos/alu/inscripciones/seleccion_materias/cargaacademica_pdf.php", requestOptions)
-            .then(response => response.text())
+            .then(response => console.log(response))
             .then(async result => {
                 console.log(result);
                 const base64Str = Buffer.from(result).toString("base64");
